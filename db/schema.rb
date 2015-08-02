cf52fb3cfb4c7e0b04f78bb007619034dd7aee5e
Jonathan Havlicek <jhavlicek91@gmail.com>
2015-08-02 15:18:18 -0400

    changes to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -14,73 +14,59 @@
 ActiveRecord::Schema.define(version: 20150731043935) do
 
   create_table "cities", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "state_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string  "name",     null: false
+    t.integer "state_id"
   end
 
   add_index "cities", ["state_id"], name: "index_cities_on_state_id"
 
   create_table "conferences", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "abbreviation"
-    t.integer  "league_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string  "name",         null: false
+    t.string  "abbreviation"
+    t.integer "league_id"
   end
 
   add_index "conferences", ["league_id"], name: "index_conferences_on_league_id"
 
   create_table "countries", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "abbreviation"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "name",         null: false
+    t.string "abbreviation"
   end
 
   create_table "divisions", force: :cascade do |t|
-    t.string   "name",          null: false
-    t.integer  "conference_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string  "name",          null: false
+    t.integer "conference_id"
   end
 
   add_index "divisions", ["conference_id"], name: "index_divisions_on_conference_id"
 
   create_table "leagues", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "abbreviation"
-    t.integer  "sport_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string  "name",         null: false
+    t.string  "abbreviation"
+    t.integer "sport_id"
   end
 
   add_index "leagues", ["sport_id"], name: "index_leagues_on_sport_id"
 
   create_table "sports", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", null: false
   end
 
   create_table "states", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "abbreviation"
-    t.integer  "country_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string  "name",         null: false
+    t.string  "abbreviation"
+    t.integer "country_id"
   end
 
   add_index "states", ["country_id"], name: "index_states_on_country_id"
 
   create_table "teams", force: :cascade do |t|
-    t.string   "name",              null: false
-    t.string   "name_abbreviation"
-    t.integer  "division_id"
-    t.integer  "city_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.string  "name",              null: false
+    t.string  "name_abbreviation"
+    t.integer "division_id"
+    t.integer "city_id"
+    t.string  "primaryColor"
+    t.string  "secondaryColor"
   end
 
   add_index "teams", ["city_id"], name: "index_teams_on_city_id"
