d548786a421012abc0d9fe1a02f16a991993f35d
Jonathan Havlicek <jhavlicek91@gmail.com>
2015-08-14 15:50:06 -0400

    set up route for players page

---
+++ ember/app/routes/player.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model(params){
+		 return this.store.findRecord('player', params.player_id).then(function(player){
+		 	return player;
+		 });
+	}
+});
